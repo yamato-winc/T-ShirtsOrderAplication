7a85bbf90c733c5f69161906cf5afed52e18060f
echizen-0629 <winc.echizen@gmail.com>
2025-07-22 11:07:07 +0900

    DTO作成

--- src/main/java/com/winc/kensyu/DTO/DesignDTO.java
+++ src/main/java/com/winc/kensyu/DTO/DesignDTO.java
@@ -1,5 +1,78 @@
 package com.winc.kensyu.DTO;
 
 public class DesignDTO {
-
+	private String orderCode;
+	private char baseColor;
+	private String Text1;
+	private int Text1Size;
+	private String Text1FontColorId;
+	private String Text2;
+	
+	
+	
+	public String getOrderCode() {
+		return orderCode;
+	}
+	public void setOrderCode(String orderCode) {
+		this.orderCode = orderCode;
+	}
+	public char getBaseColor() {
+		return baseColor;
+	}
+	public void setBaseColor(char baseColor) {
+		this.baseColor = baseColor;
+	}
+	public String getText1() {
+		return Text1;
+	}
+	public void setText1(String text1) {
+		Text1 = text1;
+	}
+	public int getText1Size() {
+		return Text1Size;
+	}
+	public void setText1Size(int text1Size) {
+		Text1Size = text1Size;
+	}
+	public String getText1FontColorId() {
+		return Text1FontColorId;
+	}
+	public void setText1FontColorId(String text1FontColorId) {
+		Text1FontColorId = text1FontColorId;
+	}
+	public String getText2() {
+		return Text2;
+	}
+	public void setText2(String text2) {
+		Text2 = text2;
+	}
+	public int getText2Size() {
+		return Text2Size;
+	}
+	public void setText2Size(int text2Size) {
+		Text2Size = text2Size;
+	}
+	public String getText2FontColorId() {
+		return Text2FontColorId;
+	}
+	public void setText2FontColorId(String text2FontColorId) {
+		Text2FontColorId = text2FontColorId;
+	}
+	public int getVerticalPosition() {
+		return verticalPosition;
+	}
+	public void setVerticalPosition(int verticalPosition) {
+		this.verticalPosition = verticalPosition;
+	}
+	public int getSidePosition() {
+		return sidePosition;
+	}
+	public void setSidePosition(int sidePosition) {
+		this.sidePosition = sidePosition;
+	}
+	private int Text2Size;
+	private String Text2FontColorId;
+	private int verticalPosition;
+	private int sidePosition;
+	
 }
