441f7fc4a58020b66def3213d0bb08db0248b212
echizen-0629 <winc.echizen@gmail.com>
2025-07-23 16:59:14 +0900

    OrderhistoryDao,Dtoの修正

--- src/main/java/com/winc/kensyu/DAO/OrderHistoryDAO.java
+++ src/main/java/com/winc/kensyu/DAO/OrderHistoryDAO.java
@@ -14,7 +14,7 @@ public class OrderHistoryDAO {
 	
 	public List<OrderHistoryDTO> getOrderHistoryDTO() {
         List<OrderHistoryDTO> list = new ArrayList<>();
-        String sql = "SELECT order_code, user_id, order_date, order_count FROM ORDER_HISTORY_TABLE where user_id = ?";
+        String sql = "SELECT orderhis.order_code, user_id, order_date, order_count, base_color,text1,text1_size,text1_fontcolor_id, text2, text2_size, text2fontcolor_id, vertical_position, side_position FROM ORDER_HISTORY_TABLE orderhis inner join DESIGN_TABLE design on orderhis.order_code = design.order_code where user_id = ? order by orderhis.order_code desc";
 
         try (Connection conn = DBAccess.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -29,6 +29,17 @@ public class OrderHistoryDAO {
                 dto.setUserId(rs.getString("user_id"));
                 dto.setOrderDate(rs.getDate("order_date"));
                 dto.setOrderCount(rs.getInt("order_count"));
+                dto.setBaseColor(rs.getString("base_color"));
+                dto.setText1(rs.getString("text1"));
+                dto.setText1Size(rs.getInt("text1_size"));
+                dto.setText1FontColorId(rs.getString("text1_fontcolor_id"));
+                dto.setText2(rs.getString("text2"));
+                dto.setText2Size(rs.getInt("text2_size"));
+                dto.setText2FontColorId(rs.getString("text2fontcolor_id"));
+                dto.setVerticalPosition(rs.getInt("vertical_position"));
+                dto.setSidePosition(rs.getInt("side_position"));
+                
+                
                 list.add(dto);
             }
              
--- src/main/java/com/winc/kensyu/DTO/OrderHistoryDTO.java
+++ src/main/java/com/winc/kensyu/DTO/OrderHistoryDTO.java
@@ -1,14 +1,76 @@
 package com.winc.kensyu.DTO;
 
 import java.sql.Date;
-import java.lang.AutoCloseable;
 
 public class OrderHistoryDTO {
 	private String orderCode;
 	private String userId;
 	private Date orderDate;
 	private int orderCount; 
+	private String baseColor;
+	private String Text1;
+	private int Text1Size;
+	private String Text1FontColorId;
+	private String Text2;
+	private int Text2Size;
+	private String Text2FontColorId;
+	private int verticalPosition;
+	private int sidePosition;
 	
+	public String getBaseColor() {
+		return baseColor;
+	}
+	public void setBaseColor(String baseColor) {
+		this.baseColor = baseColor;
+	}
+	public String getText1() {
+		return Text1;
+	}
+	public void setText1(String text1) {
+		Text1 = text1;
+	}
+	public int getText1Size() {
+		return Text1Size;
+	}
+	public void setText1Size(int text1Size) {
+		Text1Size = text1Size;
+	}
+	public String getText1FontColorId() {
+		return Text1FontColorId;
+	}
+	public void setText1FontColorId(String text1FontColorId) {
+		Text1FontColorId = text1FontColorId;
+	}
+	public String getText2() {
+		return Text2;
+	}
+	public void setText2(String text2) {
+		Text2 = text2;
+	}
+	public int getText2Size() {
+		return Text2Size;
+	}
+	public void setText2Size(int text2Size) {
+		Text2Size = text2Size;
+	}
+	public String getText2FontColorId() {
+		return Text2FontColorId;
+	}
+	public void setText2FontColorId(String text2FontColorId) {
+		Text2FontColorId = text2FontColorId;
+	}
+	public int getVerticalPosition() {
+		return verticalPosition;
+	}
+	public void setVerticalPosition(int verticalPosition) {
+		this.verticalPosition = verticalPosition;
+	}
+	public int getSidePosition() {
+		return sidePosition;
+	}
+	public void setSidePosition(int sidePosition) {
+		this.sidePosition = sidePosition;
+	}
 	public String getOrderCode() {
 		return orderCode;
 	}
